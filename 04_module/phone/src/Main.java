6d2d1c483cd6bff35e51eacbcf2d579feb3d3e63
Торадзе Диана <dianatoradze@gmail.com>
2020-04-16 22:59:39 +0500

    phone changed

--- 04_module/phone/src/Main.java
+++ 04_module/phone/src/Main.java
@@ -7,6 +7,8 @@ public class Main {
         Scanner in = new Scanner(System.in);
         System.out.println("Введите номер телефона");
         String phone = in.nextLine();
-        System.out.println(phone.replaceAll("[+]?(8|7)[-]?\\s+?\\(?(\\d{3})\\)[-]?\\s+?(\\d{3})[-]?(\\d{2})[-]?(\\d{2})", "+$1 $2 $3-$4-$5"));
+        //System.out.println(phone.replaceAll("[+]?(8|7)[-]?\\s+?\\(?(\\d{3})\\)[-]?\\s+?(\\d{3})[-]?(\\d{2})[-]?(\\d{2})", "+$1 $2 $3-$4-$5"));
+        String formattedPhone = phone.replaceAll("[+]?(\\d)(\\d{3})(\\d{3})(\\d{2})(\\d{2})", "+$1 $2 $3-$4-$5");
+        System.out.println(formattedPhone);
     }
 }
