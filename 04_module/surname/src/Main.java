7fa6b84970365f024edc658ad56f73a58296125b
Торадзе Диана <dianatoradze@gmail.com>
2020-05-13 22:51:28 +0500

    surname patternFormat

--- 04_module/surname/src/Main.java
+++ 04_module/surname/src/Main.java
@@ -1,13 +1,18 @@
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 // homework done
 public class Main {
     public static void main(String[] args) {
         final String REG_LAST_NAME = "([А-ЯЁ][а-яё]+[\\s|-]?([А-ЯЁ]?[а-яё]+))";
         final String REG_NAME = "([А-ЯЁ][а-яё]*-?([А-ЯЁ]?[а-яё]+))";
         final String REG_FULL_NAME = String.format("^%s %s %s$", REG_NAME, REG_NAME, REG_LAST_NAME);
+
         Scanner in = new Scanner(System.in);
         System.out.println("Введите слова одной строкой через пробел");
         String surname = in.nextLine();
+        Pattern paternFormat = Pattern.compile(surname);
         int count = 0;
         if (surname.length() != 0) {
             count++;
@@ -19,11 +24,14 @@ for (int i = 0; i < surname.length(); i++) {
             if (count != 3 && count != 4) {
                 System.out.println("Проверьте правильно ли введено ФИО");
             }
-        else {
+                   else {
                 String[] text = surname.split("\\s+");
                 for (int i = 0; i < text.length; i++) {
-                    count++;
-                    System.out.println(text[i].replaceAll("REG_FULL_NAME", ""));
+                        count++;
+                    Matcher m = paternFormat.matcher(text[i]);
+                    surname= m.replaceAll(REG_FULL_NAME);
+                       System.out.println(surname);
+
                 }
             }
         }
