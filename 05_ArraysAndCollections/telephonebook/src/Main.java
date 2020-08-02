112f082d1d0f8d2e7397c6384093f3f6f037573f
Торадзе Диана <dianatoradze@gmail.com>
2020-08-02 14:28:22 +0500

    regex with validator class read

---
+++ 05_ArraysAndCollections/telephonebook/src/Main.java
@@ -0,0 +1,68 @@
+import javax.swing.*;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<String, String> phoneBook = new HashMap<>();
+        phoneBook.put("Anna", "1234567890");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter command: ADD - find or enter contact, LIST - print contact ");
+        while (scanner.hasNext()) {
+            String choice = scanner.nextLine();
+            switch (choice) {
+                case "ADD":
+                    System.out.println("Enter name");
+                    String name = scanner.next();
+                    new ItemShablon(name);
+
+                    System.out.println("Enter phone number");
+                    String phoneNumber = scanner.next();
+                    new ItemShablon(phoneNumber);
+                    if (phoneBook.containsKey(name) && phoneBook.containsValue(phoneNumber)) {
+                    System.out.println("name " + name + "- number telephone "    + phoneNumber);
+                     } else {
+                        System.out.println("Add name");
+                        String nameEdit = scanner.next();
+                        new ItemShablon(nameEdit);
+                        System.out.println("Add phone number");
+                        String phoneEdit = scanner.next();
+                        new ItemShablon(phoneEdit);
+                        phoneBook.put(nameEdit, phoneEdit);
+                        print(phoneBook);
+                    }
+                    break;
+                case "LIST":
+                    for (Map.Entry <String, String> digest:phoneBook.entrySet())
+                     { //добавила, потому что выводится не весь список при добавлении нового контакта
+                         if (Validator.matcher.matches()) {
+                         System.out.println("Phone Number Valid");
+                             name= digest.getKey();
+                             phoneNumber=digest.getValue();
+                             System.out.println(name + " - " + phoneNumber);
+                         }
+                        else
+                         {
+                             System.out.println("Phone Number must be in the form");
+                         }
+                    }
+                    break;
+                case "EXIT":
+                    scanner.close();
+                    break;
+            }
+        }
+    }
+
+    private static void print(Map<String, String> map)
+    {
+        for(String key: map.keySet()) {
+            System.out.println(key + " - " +  map.get(key));
+        }
+    }
+
+}
---
+++ 05_ArraysAndCollections/telephonebook/src/Validator.java
@@ -0,0 +1,38 @@
+import java.awt.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validator {
+    private static Pattern pattern;
+    public static Matcher matcher;
+
+
+    private static final String PHONE_PATTERN =
+            "[+]?(\\d)(\\d{3})(\\d{3})(\\d{2})(\\d{2})";
+    private static  final String NAME_PATTERN = "([А-ЯЁ][а-яё]*-?([А-ЯЁ]?[а-яё]+))";
+
+    public  static boolean validateName(final String name){
+        pattern = Pattern.compile(NAME_PATTERN,Pattern.CASE_INSENSITIVE);
+        matcher = pattern.matcher(name);
+        return matcher.matches();
+    }
+
+    public  static boolean validatePhone(final String phoneNumber){
+        pattern = Pattern.compile(PHONE_PATTERN,Pattern.CASE_INSENSITIVE);
+        matcher = pattern.matcher(phoneNumber);
+        return matcher.matches();
+    }
+}
+    class ItemShablon {
+
+        {
+            Label validItemName = new Label();
+            boolean b = Validator.validateName(validItemName.getText());
+            Label validPhone = new Label();
+            boolean c = Validator.validatePhone(validPhone.getText());
+        }
+
+        public ItemShablon(String phoneNumber) {
+            
+        }
+    }
