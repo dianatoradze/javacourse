8de786a642d7b279aa6787fa276a1566f990fa33
Торадзе Диана <dianatoradze@gmail.com>
2020-11-08 23:22:01 +0500

    carnumber with time

--- 05_ArraysAndCollections/generator/src/Generated.java
+++ 05_ArraysAndCollections/generator/src/Generated.java
@@ -2,76 +2,88 @@ import java.util.*;
 
 public class Generated {
 
-    private final static String[] NUMBER_CHARS = new String[]{"C", "M", "T", "B", "A", "P", "O", "H", "E", "У"};
-
     public static void main(String[] args) {
 
-        ArrayList<String> list = new ArrayList<>(numbersGemerator());
-        Scanner scanner = new Scanner(System.in);
+        ArrayList<String> list = new ArrayList<>();
+        list.addAll(numbersGemerator());
+        Collections.sort(list);
+            Scanner scanner = new Scanner(System.in);
 
-        HashSet<String> hashSet = new HashSet<>(list);
-        TreeSet<String> treeSet = new TreeSet<>(list);
 
-        while (scanner.hasNext()) {
+            HashSet<String> hashSet = new HashSet<>();
+          //hashSet.addAll(list); попытка 1
+//            hashSet.addAll(numbersGemerator()); попытка 2
+//            hashSet.retainAll(list);
+           TreeSet<String> treeSet = new TreeSet<>();
+           treeSet.addAll(list);
 
-            System.out.println("Введите номер для поиска");
-            String number = scanner.nextLine();
+            while (true) {
 
-            long a = System.nanoTime();
-            if (list.contains(number)) {
-                System.out.println("Поиск перебором: номер найден, поиск занял: " + (System.nanoTime() - a) + "нс");
-            } else {
-                System.out.println("Поиск перебором: номер не найден, поиск занял: " + (System.nanoTime() - a) + "нс");
-            }
-            long b = System.nanoTime();
-            int search = Collections.binarySearch(list, number);
+                System.out.println("Введите номер для поиска");
+                String number = scanner.nextLine();
 
-            if (search >= 0) {
-                System.out.println("Бинарный поиск: номер найден, поиск занял: " + (System.nanoTime() - b) + "нс");
-            } else {
-                System.out.println("Бинарный поиск: номер не найден, поиск занял: " + (System.nanoTime() - b) + "нс");
-            }
-            long c = System.nanoTime();
-            if (hashSet.contains(number)) {
-                System.out.println("Поиск в HashSet: номер найден, поиск занял: " + (System.nanoTime() - c) + "нс");
-            } else {
-                System.out.println("Поиск в HashSet: номер не найден, поиск занял: " + (System.nanoTime() - c) + "нс");
-            }
-            long d = System.nanoTime();
-            if (treeSet.contains(number)) {
-                System.out.println("Поиск в TreeSet: номер найден, поиск занял: " + (System.nanoTime() - d) + "нс");
-            } else {
-                System.out.println("Поиск в TreeSet: номер не найден, поиск занял: " + (System.nanoTime() - d) + "нс");
+                long start = System.nanoTime();
+                long resultTime = 0;
+                if (list.contains(number)) {
+                    resultTime = System.nanoTime() - start;
+                    System.out.println("Поиск перебором: номер найден, поиск занял: " + resultTime + " нс");
+                } else {
+                    System.out.println("Поиск перебором: номер не найден, поиск занял: " + resultTime + " нс");
+                }
+                long b = System.nanoTime();
+
+                int search = Collections.binarySearch(list, number);
+
+                if (search >= 0) {
+                    resultTime = System.nanoTime() - b;
+                    System.out.println("Бинарный поиск: номер найден, поиск занял: " + resultTime + " нс");
+                } else {
+                    System.out.println("Бинарный поиск: номер не найден, поиск занял: " + resultTime + " нс");
+                }
+                long c = System.nanoTime();
+               ;
+                if (hashSet.contains(number)) {
+                    resultTime = System.nanoTime() - c;
+                    System.out.println("Поиск в HashSet: номер найден, поиск занял: " + resultTime + " нс");
+                } else {
+                    System.out.println("Поиск в HashSet: номер не найден, поиск занял: " + resultTime + " нс");
+                }
+                long d = System.nanoTime();
+
+                if (treeSet.contains(number)) {
+                    resultTime = System.nanoTime() - d;
+                    System.out.println("Поиск в TreeSet: номер найден, поиск занял: " + resultTime + " нс");
+                } else {
+                    System.out.println("Поиск в TreeSet: номер не найден, поиск занял: " + resultTime + " нс");
+                }
             }
         }
-    }
 
-    private static Collection<? extends String> numbersGemerator() {
+        public static ArrayList <String> numbersGemerator() {
 
-        int regionCode = 1;
-        List<String> result = new ArrayList<>();
-        while (regionCode < 200) {
-            for (String x : NUMBER_CHARS) {
-                for (String y : NUMBER_CHARS) {
-                    for (String z : NUMBER_CHARS) {
-                        for (int j = 1; j < 1000; j++) {
-                            if (regionCode < 100) {
-                                result.add(String.format("%s%03d%s%s%02d", x, j, y, z, regionCode));
-                            } else {
-                                result.add(String.format("%s%03d%s%s%03d", x, j, y, z, regionCode));
+            int regionCode = 1;
+            ArrayList<String> result = new ArrayList<>();
+            final  String[] NUMBER_CHARS = new String[]{"C", "M", "T", "B", "A", "P", "O", "H", "E", "У"};
+
+            while (regionCode < 200) {
+                for (String x : NUMBER_CHARS) {
+                    for (String y : NUMBER_CHARS) {
+                        for (String z : NUMBER_CHARS) {
+                            for (int j = 1; j < 100; j++) {
+                                if (regionCode < 100) {
+                                    result.add(String.format("%s%03d%s%s%02d", x, j, y, z, regionCode));
+                                } else {
+                                    result.add(String.format("%s%03d%s%s%03d", x, j, y, z, regionCode));
+                                }
                             }
                         }
                     }
                 }
+                regionCode++;
             }
-            regionCode++;
+            return result;
         }
-        return result;
     }
-
-}
-
-
 
 
 
