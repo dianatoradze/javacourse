6bf4ae38facabe472d7d9c0a77f8f6d5a6a4d0f2
Торадзе Диана <dianatoradze@gmail.com>
2020-12-06 17:53:24 +0500

    BankAccount with class main

---
+++ 06_InheritanceAndPolymorphism/Bank/src/Main.java
@@ -0,0 +1,15 @@
+import accounts.Account;
+
+public class Main {
+    public static void main(String[] args) {
+        Account bill = new Account(1001);
+        bill.deposit(200);
+        bill.withdrawal(50);
+        System.out.println("Итоговый баланс: " + bill.getBalance());
+        Account billSend = new Account(1002);
+        billSend.deposit(75.50);
+        billSend.withdrawal(20.00);
+        System.out.println("Итоговый баланс: " + billSend.getBalance());
+        System.out.println("Перевод средств " + bill.send(bill, 100));
+    }
+}
