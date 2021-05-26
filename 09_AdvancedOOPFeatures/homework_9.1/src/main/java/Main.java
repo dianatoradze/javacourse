e806e9b3eb058ca159ff0cb2424cb97f4d762e5c
Торадзе Диана <dianatoradze@gmail.com>
2021-05-27 00:14:12 +0500

    homework 9.1 check

--- 09_AdvancedOOPFeatures/homework_9.1/src/main/java/Main.java
+++ 09_AdvancedOOPFeatures/homework_9.1/src/main/java/Main.java
@@ -15,7 +15,8 @@ public class Main {
 
     public static void sortBySalaryAndAlphabet(List<Employee> staff) {
         //TODO Метод должен отсортировать сотрудников по заработной плате и алфавиту.
-        //Collections.sort(staff, (o1, o2) -> o1.getSalary().compareTo(o2.getSalary()));
+       // Collections.sort(staff, (o1, o2) -> o1.getSalary().compareTo(o2.getSalary()));
+
         staff.sort((o1, o2) -> {
 
             sortByAlphabet(staff, o1, o2);
@@ -28,8 +29,7 @@ public class Main {
     private static void sortByAlphabet(List<Employee> staff, Employee o1, Employee o2) {
         if (o1.getSalary().equals(o2.getSalary())) {
 
-            Collections.sort(staff, Comparator.comparing(Employee::getName));
-
+            staff.sort(Comparator.comparing(Employee::getName));
         }
 //        else {
 //            Collections.sort(staff, Comparator.comparingInt(Employee::getSalary));
