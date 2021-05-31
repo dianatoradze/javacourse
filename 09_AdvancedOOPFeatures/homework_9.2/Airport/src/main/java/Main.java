1801c8d7ebed9e655687ff361d76814aa1e50318
Торадзе Диана <dianatoradze@gmail.com>
2021-05-31 23:18:55 +0500

    homework 9.1 done

--- 09_AdvancedOOPFeatures/homework_9.2/Airport/src/main/java/Main.java
+++ 09_AdvancedOOPFeatures/homework_9.2/Airport/src/main/java/Main.java
@@ -1,8 +1,13 @@
+import com.skillbox.airport.Aircraft;
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
+import com.skillbox.airport.Flight.Type;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+
+import static com.skillbox.airport.Flight.Type.valueOf;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,7 +17,21 @@ public class Main {
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
         //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
-        return Collections.emptyList();
+//                    terminal.addFlight(this.generateFlight(Type.DEPARTURE));
+        airport.getAllAircrafts().stream().flatMap((Aircraft t) -> Flight.getDate(t))
+                .stream().forEach(e-> {
+            //return System.out.println(valueOf(String.valueOf(Type.DEPARTURE)));
+
+        });
+
+        //airport.getAllAircrafts().stream().map(DEPARTURE))
+.collect(Collectors.groupingBy(Type.DEPARTURE));
+
+
+                        //toList().forEach(System.out::println);
+
+        return Collections.list();
+                //Collections.emptyList();
     }
 
 }
