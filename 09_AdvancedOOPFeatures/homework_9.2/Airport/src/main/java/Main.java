bf8b24cb59629402d1577c69a3bb5912b9b2a3ed
Торадзе Диана <dianatoradze@gmail.com>
2021-06-27 17:23:24 +0500

    homework airport check

--- 09_AdvancedOOPFeatures/homework_9.2/Airport/src/main/java/Main.java
+++ 09_AdvancedOOPFeatures/homework_9.2/Airport/src/main/java/Main.java
@@ -1,21 +1,36 @@
+import com.skillbox.airport.Aircraft;
 import com.skillbox.airport.Airport;
 import com.skillbox.airport.Flight;
+import com.skillbox.airport.Terminal;
 
-import java.util.Collections;
-import java.util.List;
+import java.text.SimpleDateFormat;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
-
+        System.out.println(findPlanesLeavingInTheNextTwoHours(Airport.getInstance()));
 
     }
 
     public static List<Flight> findPlanesLeavingInTheNextTwoHours(Airport airport) {
         //TODO Метод должден вернуть список рейсов вылетающих в ближайшие два часа.
+        long date = new Date().getTime();
+        Calendar calendar = Calendar.getInstance();
 
+//        Date time = calendar.getTime();
+//        calendar.roll(Calendar.HOUR_OF_DAY, 2);
+//        Date hours = calendar.getTime();
 
-        
-        return Collections.emptyList();
+        return airport.getTerminals()
+                .stream()
+                .map(Terminal::getFlights)
+                .flatMap(Collection::stream)
+                .filter(flight -> flight.getType().equals(Flight.Type.DEPARTURE))
+                .filter(e -> e.getDate().getTime() >= date
+                        && e.getDate().getTime() < (date + 7200000))
+                .collect(Collectors.toList());
+
     }
 
 }
