2559c01e81f38fb5736b04f1acc85daa2b496fc7
Торадзе Диана <dianatoradze@gmail.com>
2021-07-14 20:36:05 +0500

    homework 12.1 done

--- 10_ExceptionsDebuggingAndTesting/homework_10.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
+++ 10_ExceptionsDebuggingAndTesting/homework_10.1/ConsoleCustomerList/src/main/java/CustomerStorage.java
@@ -1,5 +1,9 @@
+
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class CustomerStorage {
     private final Map<String, Customer> storage;
@@ -8,18 +12,27 @@ public class CustomerStorage {
         storage = new HashMap<>();
     }
 
-    public void addCustomer(String data) {
+    public void addCustomer(String data) throws IllegalArgumentException {
         final int INDEX_NAME = 0;
         final int INDEX_SURNAME = 1;
         final int INDEX_EMAIL = 2;
         final int INDEX_PHONE = 3;
 
         String[] components = data.split("\\s+");
+        if (components.length != 4) {
+            throw new IllegalArgumentException("Error format");
+        }
+
+        if (!isEmailValid(components[INDEX_EMAIL])) throw new IllegalArgumentException();
+        if (!isPhoneNumberValid(components[INDEX_PHONE])) {
+            throw new IllegalArgumentException();
+        }
         String name = components[INDEX_NAME] + " " + components[INDEX_SURNAME];
         storage.put(name, new Customer(name, components[INDEX_PHONE], components[INDEX_EMAIL]));
     }
 
-    public void listCustomers() {
+    public void listCustomers() throws IOException {
+
         storage.values().forEach(System.out::println);
     }
 
@@ -34,4 +47,24 @@ public class CustomerStorage {
     public int getCount() {
         return storage.size();
     }
+
+    public static boolean isPhoneNumberValid(String phone) {
+
+        String regex = "^((8|\\+7)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{7,10}$";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(phone);
+        return matcher.matches();
+    }
+
+    public static boolean isEmailValid(String email) {
+
+        String regexEmail = "^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$";
+
+        Pattern p = Pattern.compile(regexEmail, Pattern.CASE_INSENSITIVE);
+        Matcher m = p.matcher(email);
+        return m.matches();
+    }
+
 }
+
