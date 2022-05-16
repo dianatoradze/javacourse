530d7c064e52990dcc99f9178537cecbbaf61206
dianatoradze <dianatoradze1706@gmail.com>
2022-05-16 14:12:05 +0500

    homework checked

--- FilesAndNetwork/homework_2/src/main/java/FileUtils.java
+++ FilesAndNetwork/homework_2/src/main/java/FileUtils.java
@@ -1,5 +1,34 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 public class FileUtils {
     public static void copyFolder(String sourceDirectory, String destinationDirectory) {
         // TODO: write code copy content of sourceDirectory to destinationDirectory
-    }
+            try {
+                Path directory1 = Paths.get(sourceDirectory);
+                Path directory2 = Paths.get(destinationDirectory);
+
+                List<Path> source = Files.walk(directory1).collect(toList());
+                List<Path> destination = new ArrayList<>();
+                for (Path path : source) {
+                    Path relativize = directory1.relativize(path);
+                    Path resolve = directory2.resolve(relativize);
+                    destination.add(resolve);
+                }
+
+                for (int i = 0; i < source.size(); i++) {
+                    Files.copy(source.get(i), destination.get(i), StandardCopyOption.REPLACE_EXISTING);
+                }
+                System.out.println("Файлы скопированы!");
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
 }
--- FilesAndNetwork/homework_2/src/main/java/Main.java
+++ FilesAndNetwork/homework_2/src/main/java/Main.java
@@ -1,4 +1,38 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+    private static InputStreamReader path;
     public static void main(String[] args) {
+        //C:\Users\diana\Documents\Java\Коды
+        path = new InputStreamReader(System.in);
+
+        BufferedReader reader = new BufferedReader(path);
+
+        for (; ; ) {
+            System.out.println("Введите путь к папке которую хотите скопировать:");
+            File sourceFile = null;
+            try {
+                sourceFile = new File(reader.readLine());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (!sourceFile.exists()) {
+                System.out.println("Файл отсутствует!");
+                continue;
+            }
+            System.out.println("Введите путь:");
+            File destinationFile = null;
+            try {
+                destinationFile = new File(reader.readLine() + File.separator + sourceFile.getName());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            if (!destinationFile.exists()) destinationFile.mkdirs();
+
+            FileUtils.copyFolder(String.valueOf(sourceFile),String.valueOf(destinationFile));
+        }
     }
 }
